f8efff787890ac3d31ea140f1ff89a0684c5f299
guita13921 <guita13921@gmail.com>
2022-01-19 18:13:49 +0700

    start again

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 int main(){
-int N=10,M=10;
-char seat[N][M];
-for(int i=0; i<N ;i++){
-    for(int j=0; j<M ; j++){
-        if(j%3==0)seat[i][j] = 'X';
-        else seat[i][j]= 'O';
-    }
-}
-cout << seat[0][0];
+
 return 0;
 }
