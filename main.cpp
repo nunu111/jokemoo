09267721cd340ac2320e09e3850ded8fbf9c6373
nunu111 <nunu111za@hotmail.com>
2022-01-20 10:30:55 +0700

    add check_seat

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<cstdlib>
 using namespace std;
-
+//สร้างไฟล์ขึ้นมาเพื่อจัดเก็บข้อมูลที่นั่ง * ย้ำว่าใช้สร้างเท่านั้นหากใช้ใหม่ข้อมูลเก่าจะหาย *
 void create_movieseat(string movie_name,int N,int M){
-    ofstream seatmovie ("selected_seat.txt");
+    ofstream seatmovie (movie_name);
     seatmovie << movie_name <<endl;
-    for(int i =0;i <N;i++){
-        for(int j =0;j <M;j++){
-            seatmovie << "0" << " ";
-        }
-        seatmovie << endl;
+    for(int i =0;i <N*M;i++){
+        seatmovie << "0" << "\n";
     }
+    seatmovie.close();
+}
+//ยังไม่เสร็จ
+void check_seat(string movie_name){
+    string name,num;
+    int i;
+    ifstream check (movie_name);
+    do
+    {
+       getline(check,name);
+       i++;
+       if (i >200) break;
+    } while (movie_name != name);
+    while(getline(check,num)){
+        if( num == "0" ) cout << 'O';
+        else if ( num == "1" ) cout << 'X';
+        else;
+    }
+    check.close();
 }
 
 void seat(char a[],int N,int M){
@@ -30,6 +47,7 @@ int main(){
     int M = 5;
     int N = 5;
     create_movieseat("matrix",N,M);
+    check_seat("matrix");
     char block[(N*M)+(N+M)];
     seat(block,(N*M)+(N+M),M+2);
     return 0;
