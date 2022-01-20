0b3a955749c4448fd7056d44314a138d80e0eafd
nunu111 <nunu111za@hotmail.com>
2022-01-20 09:48:45 +0700

    add create_movieseat

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
+
+void create_movieseat(string movie_name,int N,int M){
+    ofstream seatmovie ("selected_seat.txt");
+    seatmovie << movie_name <<endl;
+    for(int i =0;i <N;i++){
+        for(int j =0;j <M;j++){
+            seatmovie << "0" << " ";
+        }
+        seatmovie << endl;
+    }
+}
 
 void seat(int N,int M){
     char seat[N][M];
@@ -13,6 +26,7 @@ void seat(int N,int M){
 }
 
 int main(){
+create_movieseat("matrix",10,10);
 seat(20,20);
 return 0;
 }
