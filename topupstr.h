2d8b099fd5409862f8fd79267e3b83edaae9dbc2
nunu111 <nunu111za@hotmail.com>
2022-02-12 20:58:27 +0700

    clear

--- topupstr.h
+++
@@ -1,9 +0,0 @@
-#include<string>
-#include<iostream>
-using namespace std;
-
-string toUpperStr(string x){ //code เปลี่ยนตัวเป็นตัวใหญ่ทั้งหมด ของอาจารย์เห็นว่ามันใช้ได้
-    string y = x;
-    for(unsigned i = 0; i < x.size();i++) y[i] = toupper(x[i]);
-    return y;
-}
